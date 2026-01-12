6e269bc2954f32e1643e300354cf1e08287296be
anonymmized <lih0deev@inbox.ru>
2026-01-13 00:14:35 +0300

    calculation of solar time of sunrise and sunset and corrections for longitude

--- calculations.c
+++ calculations.c
@@ -11,15 +11,39 @@ double to_rad(double degree) {
     return (3.14 * degree) / 180;
 }
 
+double calc_h0(unsigned int day, double lat) {
+    double declination = to_rad(-23.44 * cos((2 * pi * 22) / 365));
+    double lat_rad = to_rad(lat);
+    double h0 = a_cos((-1) * tan(lat_rad) * tan(declination));
+    return h0;
+}
+
+double day_len(double h0) {
+    return (2 * h0) / to_rad(15.0);
+}
+
 int main() {
-    double declination = -23.44 * cos((2 * pi * 22)/365);
-    double decl_rad = to_rad(declination);
-    double lat_rad = to_rad(55.75);
-    printf("Declination for 12 jan: %.4f\n", declination);
-    double cos_h0 = (-1) * tan(lat_rad) * tan(decl_rad);
-    double h0 = a_cos(cos_h0);
-    printf("Cos h0: %.4f\n", cos_h0);
-    printf("h0: %.4f\n", h0);
+    unsigned int day;
+    double lat, lon;
+    int tz;
+    printf("Enter day: ");
+    scanf("%d", &day);
+    printf("Enter latitude: ");
+    scanf("%lf", &lat);
+    printf("Enter longitude: ");
+    scanf("%lf", &lon);
+    printf("Enter time zone: ");
+    scanf("%d", &tz);
+    double h0 = calc_h0(day, lat);
+    printf("H0 for moscow %d day: %.4f\n", day, h0);
+    double day_len = (2 * h0) / to_rad(15.0);
+    double sunrise_solar = 12 - (h0 / to_rad(15.0));
+    double sunset_solar = 12 + (h0 / to_rad(15.0));
+    printf("Day length: %.2f\n", day_len);
+    printf("Sunrise (solar): %.2f\nSunset (solar): %.2f\n", sunrise_solar, sunset_solar);
+    double lon_merid = 15.0 * tz;
+    double lon_correction = 4 * (lon_merid - lon);
+    printf("longitude correction: %.2f\n", lon_correction);
     return 0;
 }
 
