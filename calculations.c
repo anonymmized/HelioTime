a3e8eff5618e2cd996af6df856041fc94ea339bf
anonymmized <lih0deev@inbox.ru>
2026-01-13 20:30:44 +0300

    final calculations with variables

--- calculations.c
+++ calculations.c
@@ -8,13 +8,16 @@ double a_cos(double x) {
 }
 
 double to_rad(double degree) {
-    return (3.14 * degree) / 180;
+    return (M_PI * degree) / 180;
 }
 
-double calc_h0(unsigned int day, double lat) {
-    double declination = to_rad(-23.44 * cos((2 * pi * 22) / 365));
+double calc_h0(int day, double lat) {
     double lat_rad = to_rad(lat);
-    double h0 = a_cos((-1) * tan(lat_rad) * tan(declination));
+    double gamma = (2.0 * M_PI / 365.0) * (day - 81);
+    double declination = to_rad(23.44) * sin(gamma);
+
+    double cosH0 = -tan(lat_rad) * tan(declination);
+    double h0 = a_cos(cosH0);
     return h0;
 }
 
@@ -22,18 +25,24 @@ double day_len(double h0) {
     return (2 * h0) / to_rad(15.0);
 }
 
+double eot_calc(int day) {
+    double B = (360.0 / 365.0) * (day - 81);
+    double Brad = to_rad(B);
+    return 9.87 * sin(2.0 * Brad) - 7.53 * cos(Brad) - 1.5 * sin(Brad);
+}
+
 int main() {
-    unsigned int day;
-    double lat, lon;
-    int tz;
+    int day;
+    double lat = 55.75, lon = 37.62;
+    int tz = 3;
     printf("Enter day: ");
     scanf("%d", &day);
-    printf("Enter latitude: ");
-    scanf("%lf", &lat);
-    printf("Enter longitude: ");
-    scanf("%lf", &lon);
-    printf("Enter time zone: ");
-    scanf("%d", &tz);
+    //printf("Enter latitude: ");
+    //scanf("%lf", &lat);
+    //printf("Enter longitude: ");
+    //scanf("%lf", &lon);
+    //printf("Enter time zone: ");
+    //scanf("%d", &tz);
     double h0 = calc_h0(day, lat);
     printf("H0 for moscow %d day: %.4f\n", day, h0);
     double day_len = (2 * h0) / to_rad(15.0);
@@ -44,6 +53,25 @@ int main() {
     double lon_merid = 15.0 * tz;
     double lon_correction = 4 * (lon_merid - lon);
     printf("longitude correction: %.2f\n", lon_correction);
+    double eot = eot_calc(day);
+    printf("eot: %.2f\n", eot);
+    double C = lon_correction - eot;
+    double t_sunrise = sunrise_solar + (C / 60.0);
+    int hours1 = (int)t_sunrise;
+    int minutes1 = round((t_sunrise - hours1) * 60);
+    if (minutes1 == 60) {
+        minutes1 = 0;
+        hours1 += 1;
+    }
+
+    double t_sunset = sunset_solar + (C / 60.0);
+    int hours2 = (int)t_sunset;
+    int minutes2 = round((t_sunset - hours2) * 60.0);
+    if (minutes2 == 60) {
+        minutes2 = 0;
+        hours2 += 1;
+    }
+    printf("%d - Sunrise - %02d:%02d\n%d - Sunset - %02d:%02d\n", day, hours1, minutes1, day, hours2, minutes2);
     return 0;
 }
 
